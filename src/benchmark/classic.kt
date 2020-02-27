afcd9172632295f4bebc21feecae353e747e3c01
Rares Dima <rares.dima96@yahoo.com>
2020-02-27 11:59:49 +0200

    Added documentation for the benchmarks package.
    Signed-off-by: Rares Dima(uidn7122) <rares.dima@continental-corporation.com>

--- src/benchmark/classic.kt
+++ src/benchmark/classic.kt
@@ -3,8 +3,19 @@ package benchmark
 import kotlin.math.PI
 import kotlin.math.cos
 
-fun rastrigin(x: List<Double>): Double {
+/**
+ * The Rastrigin function.
+ *
+ * This is a classic multimodal function used in optimization tasks.
+ * R(x) = A n + SUM( xi^2 - A cos(2 pi xi) )
+ * With A = 10 and n being the size of the input.
+ *
+ * @param x The function arguments.
+ *
+ * @return The value of the Rastrigin function R(x).
+ */
+fun rastrigin(x: Iterable<Double>): Double {
     val A = 10
-    val n = x.size
+    val n = x.toList().size
     return A * n + x.sumByDouble { xi -> xi * xi - A * cos(2 * PI * xi) }
 }
