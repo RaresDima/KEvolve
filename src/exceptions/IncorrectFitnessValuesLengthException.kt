886be6ffa1d4a9e3dc4290fc7ec892d469fd16e0
Rares Dima <rares.dima96@yahoo.com>
2020-02-27 12:03:10 +0200

    Added documentation for the exceptions package.
    Signed-off-by: Rares Dima(uidn7122) <rares.dima@continental-corporation.com>

--- src/exceptions/IncorrectFitnessValuesLengthException.kt
+++ src/exceptions/IncorrectFitnessValuesLengthException.kt
@@ -1,3 +1,9 @@
 package exceptions
 
+import fitness.MultiObjectiveFitness
+
+/**
+ * Thrown when the number of values to be inserted into a [MultiObjectiveFitness]
+ * is either too long or too short.
+ */
 class IncorrectFitnessValuesLengthException(msg: String): Exception(msg)
