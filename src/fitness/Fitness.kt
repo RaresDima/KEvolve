48c19f01c527a0713e0b8e6f887fb2a1e25ab7e8
Rares Dima <rares.dima96@yahoo.com>
2020-02-27 12:31:23 +0200

    Added documentation for the Fitness class.
    Signed-off-by: Rares Dima(uidn7122) <rares.dima@continental-corporation.com>

--- src/fitness/Fitness.kt
+++ src/fitness/Fitness.kt
@@ -1,8 +1,10 @@
 package fitness
 
-import java.lang.IllegalArgumentException
-import kotlin.math.sign
-
+/**
+ * Single-objective fitness.
+ *
+ * @property value The objective value.
+ */
 class Fitness: BaseFitness() {
     var value: Double = Double.NEGATIVE_INFINITY
         set(value) {
