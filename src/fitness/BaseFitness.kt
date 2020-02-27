5c36254136709ae18759c7c61951e284e08cf37b
Rares Dima <rares.dima96@yahoo.com>
2020-02-27 12:29:23 +0200

    Added documentation for the BaseFitness class.
    Signed-off-by: Rares Dima(uidn7122) <rares.dima@continental-corporation.com>

--- src/fitness/BaseFitness.kt
+++ src/fitness/BaseFitness.kt
@@ -3,19 +3,53 @@ package fitness
 import java.lang.IllegalArgumentException
 import kotlin.math.sign
 
+/**
+ * The Fitness class that exposes the functionalities all Fitness classes should
+ * have.
+ *
+ * @property valid
+ *  Whether this Fitness object contains a valid fitness value or the
+ *  individual that owns this Fitness needs to be evaluated again.
+ */
 abstract class BaseFitness {
     var valid: Boolean = false
 
+    /**
+     * Invalidates this Fitness object, requiring the corresponding individual to be
+     * evaluated again.
+     */
     fun invalidate() { valid = false }
 
+    /**
+     * Retrieves the value of this Fitness object. This is a single value.
+     * For multi-objective purposes this might be computed using a weighted sum of the
+     * objective values.
+     */
     abstract fun value(): Double
 
+    /**
+     * Checks if the [value] of a Fitness object is  the same as that of [other].
+     *
+     * @param other The Fitness object to compare this to.
+     *
+     * @return Whether the 2 Fitness objects have the same value.
+     */
     override operator fun equals(other: Any?): Boolean =
         if (other is BaseFitness)
             this.value() == other.value()
         else
             throw IllegalArgumentException("Can only compare to another class derived from BaseFitness.")
 
+    /**
+     * Compares the [value] of 2 Fitness objects.
+     *
+     * @param other The Fitness object to compare this to.
+     *
+     * @return
+     *  - 1  if this Fitness is greater than [other].
+     *  - -1 if this Fitness is lower than [other].
+     *  - 0 if this and [other] are equal.
+     */
     operator fun compareTo(other: Any?): Int =
         if (other is BaseFitness)
             this.value().minus(other.value())
