00f1a10b69e364787e1aff641be96b5e216d839e
Rares Dima <rares.dima96@yahoo.com>
2020-02-27 12:12:15 +0200

    Added documentation for the extensions.boolean package.
    Signed-off-by: Rares Dima(uidn7122) <rares.dima@continental-corporation.com>

--- src/extensions/boolean.kt
+++ src/extensions/boolean.kt
@@ -1,9 +1,43 @@
 package extensions
 
+/**
+ * Converts the [Boolean] to a [Byte]
+ *
+ * @return 1 if the [Boolean] is true, else 0.
+ */
 fun Boolean.toByte():  Byte  = if (this) 1 else 0
+
+/**
+ * Converts the [Boolean] to a [Short]
+ *
+ * @return 1 if the [Boolean] is true, else 0.
+ */
 fun Boolean.toShort(): Short = if (this) 1 else 0
+
+/**
+ * Converts the [Boolean] to an [Int]
+ *
+ * @return 1 if the [Boolean] is true, else 0.
+ */
 fun Boolean.toInt():   Int   = if (this) 1 else 0
+
+/**
+ * Converts the [Boolean] to a [Long]
+ *
+ * @return 1 if the [Boolean] is true, else 0
+ */
 fun Boolean.toLong():  Long  = if (this) 1 else 0
 
+/**
+ * Converts the [Boolean] to a [Float]
+ *
+ * @return 1.0 if the [Boolean] is true, else 0.0
+ */
 fun Boolean.toFloat():  Float  = if (this) 1f  else 0f
+
+/**
+ * Converts the [Boolean] to a [Double]
+ *
+ * @return 1.0 if the [Boolean] is true, else 0.0
+ */
 fun Boolean.toDouble(): Double = if (this) 1.0 else 0.0
