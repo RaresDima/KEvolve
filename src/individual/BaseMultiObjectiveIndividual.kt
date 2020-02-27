2f4331f8ee93aeeb7691e9a87cf5a81c755f7b55
Rares Dima <rares.dima96@yahoo.com>
2020-02-27 12:49:30 +0200

    Added documentation for the MultiObjectiveIndividual class. Finished documentation for the individual package.
    Signed-off-by: Rares Dima(uidn7122) <rares.dima@continental-corporation.com>

--- src/individual/BaseMultiObjectiveIndividual.kt
+++ src/individual/BaseMultiObjectiveIndividual.kt
@@ -3,6 +3,20 @@ package individual
 import extensions.delegates.AssignOnce
 import fitness.MultiObjectiveFitness
 
+/**
+ * Multi-objective Individual class.
+ *
+ * All classes used as individuals for multi-objective optimization should inherit
+ * from this using the default, no argument constructor.
+ *
+ * E.g. `class MyIndividual(...): BaseMultiObjectiveIndividual() {...}`
+ *
+ * Inheriting from this class is necessary for the [MultiObjectiveFitness]
+ * creation and initialization to be handled automatically, with no further action
+ * on the matter required by the user.
+ *
+ * @property fitness The [MultiObjectiveFitness] object of this individual.
+ */
 abstract class BaseMultiObjectiveIndividual {
     var fitness: MultiObjectiveFitness by AssignOnce()
 }
