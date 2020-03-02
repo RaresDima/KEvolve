14b77b7c353b58c560ee27b9dd285ef9f39cce51
Rares Dima <rares.dima96@yahoo.com>
2020-03-02 14:11:31 +0200

    Added BaseSelection.
    Signed-off-by: Rares Dima(uidn7122) <rares.dima@continental-corporation.com>

--- src/selection/BaseSelection.kt
+++ src/selection/BaseSelection.kt
@@ -3,8 +3,18 @@ package selection
 import individual.BaseIndividual
 
 /**
- *
+ * Base Selection class that exposes functionalities all Selection algorithms
+ * should have.
  */
 abstract class BaseSelection<INDIVIDUAL: BaseIndividual<*>> {
-    abstract fun invoke(pop: List<INDIVIDUAL>): List<INDIVIDUAL>
+
+    /**
+     * Select the desired number of individuals.
+     *
+     * @param pop The population to select from.
+     * @param k How many individuals to select.
+     *
+     * @return A [List] with the selected individuals.
+     */
+    abstract fun invoke(pop: List<INDIVIDUAL>, k: Int): List<INDIVIDUAL>
 }
