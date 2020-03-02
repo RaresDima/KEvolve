a7ae12949ffe083993600bcde288504a7a0efb64
Rares Dima <rares.dima96@yahoo.com>
2020-03-02 15:30:37 +0200

    Added Tournament selection tests.
    Signed-off-by: Rares Dima(uidn7122) <rares.dima@continental-corporation.com>

--- src/selection/Tournament.kt
+++ src/selection/Tournament.kt
@@ -6,6 +6,22 @@ import exceptions.selection.SelectionTooSmallException
 import extensions.randomSequence
 import individual.BaseIndividual
 
+/**
+ * Tournament Selection selects the best individual from a small pool of randomly
+ * chosen [tournSize] individuals.
+ *
+ * This repeats k times (until the desired number of individuals are selected).
+ *
+ * @property tournSize The size of each tournament pool.
+ *
+ * @constructor
+ * @param tournSize The size of each tournament pool.
+ *
+ * @throws InvalidTournamentSizeException
+ * If [tournSize] < 2. Since that would make each tournament either to small to be
+ * able to choose from (no point in choosing from a tournament of 1 individual) or
+ * have a size of 0 or less.
+ */
 class Tournament<INDIVIDUAL: BaseIndividual<*>>(val tournSize: Int): BaseSelection<INDIVIDUAL>() {
 
     init {
@@ -13,6 +29,22 @@ class Tournament<INDIVIDUAL: BaseIndividual<*>>(val tournSize: Int): BaseSelecti
             throw InvalidTournamentSizeException("tournSize = $tournSize < 2")
     }
 
+    /**
+     * Tournament Selection selects the best individual from a small pool of randomly
+     * chosen [tournSize] individuals.
+     *
+     * This repeats [k] times (until the desired number of individuals are selected).
+     *
+     * @param pop The population to select from.
+     * @param k The number of individuals to select.
+     *
+     * @throws PopulationTooSmallException
+     * If the size of the population is less than [tournSize] since that would mean
+     * that there are not enough individuals to from a tournament.
+     *
+     * @throws SelectionTooSmallException
+     * If [k] < 1 since it makes no sense to select 0 individuals.
+     */
     override operator fun invoke(pop: List<INDIVIDUAL>, k: Int): List<INDIVIDUAL> {
 
         if (pop.size < tournSize)
