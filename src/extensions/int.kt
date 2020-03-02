1e0d59cb70e032442b7eb8af1a7eb7ab962faab1
Rares Dima <rares.dima96@yahoo.com>
2020-03-02 14:25:38 +0200

    Added random int sequence.
    Signed-off-by: Rares Dima(uidn7122) <rares.dima@continental-corporation.com>

--- src/extensions/int.kt
+++ src/extensions/int.kt
@@ -2,6 +2,13 @@ package extensions
 
 import kotlin.random.Random
 
+/**
+ * Returns a [Sequence] of [Int]s from [this] range.
+ *
+ * @param random The [Random] instance to use (optional).
+ *
+ * @return A [Sequence] of [Int]s from [this] range.
+ */
 fun IntRange.randomSequence(random: Random = Random.Default): Sequence<Int> = sequence {
     while(true) {
         yield(this@randomSequence.random(random))
