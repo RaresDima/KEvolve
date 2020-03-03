9988a929727282c57a9d2b42ade42cd67653a85d
Rares Dima <rares.dima96@yahoo.com>
2020-03-03 16:14:29 +0200

    Added basic algorithm pseudocode.
    Signed-off-by: Rares Dima(uidn7122) <rares.dima@continental-corporation.com>

--- src/main.kt
+++ src/main.kt
@@ -4,6 +4,12 @@ import population.PopulationFactory
 import selection.SelectTournament
 import utils.BinaryRepresentation
 import utils.Bit
+import kotlin.random.Random
+
+
+
+//val offspringPairs = offspring.slice(0 until offspring.lastIndex step 2)  // (1,2), (3,4), (5,6), ...
+//    .zip(offspring.slice(1 until offspring.lastIndex step 2))
 
 
 fun main(args: Array<String>) {
@@ -19,49 +25,67 @@ fun main(args: Array<String>) {
 
     val TOURN_SIZE = 3
 
-    // Single number representation
+    val CX_PB = 0.5
+    val MUT_PB = 0.1
 
-    val repr = BinaryRepresentation(MIN, MAX, DIGITS)
+    val repr = BinaryRepresentation(MIN, MAX, DIGITS)  // Handles Conversions: Binary <-> Real
 
-    // Fitness
+    class RastriginIndividual(val bits: MutableList<Bit>): Individual()  // Just inherit Individual()
 
-    val fitness = Rastrigin()
-
-    // Individual and Population
-
-    class RastriginIndividual(val bits: MutableList<Bit>): Individual()
-
-    val popFactory = PopulationFactory {
+    val popFactory = PopulationFactory {  // Keyword-like initialization definition.
         RastriginIndividual(
             MutableList(DIMS * repr.nBits) { Bit.random() }
         )
     }
 
-    val pop = popFactory.spawn(POP_SIZE)
+    // Operators are instantiated and then used as functions.
 
-    // Selection
+    val fitness = Rastrigin()
 
     val select = SelectTournament(tournSize = TOURN_SIZE)
 
-    // Crossover
-
     val crossover = CrossoverOnePoint { it.bits }
 
-    // Mutation
-
-    val mutate = MutateFlipBit(bitpb = 0.1)
+    val mutate = MutateFlipBit(bitpb = 0.1) { it.bits }
 
     // GA
 
+    val pop = popFactory.spawn(POP_SIZE)  // Create the population.
+
     for (ind in pop)
-        ind.fitness.value = fitness(repr.splitToReal(ind.bits))
+        ind.fitness.value = fitness(repr.splitToReal(ind.bits))  // Evaluate the population
 
     for (g in 1..100) {
 
-        val offspring = select(pop, POP_SIZE)
+        var offspring = select(pop, POP_SIZE)  // Selection
 
+        offspring = popFactory.clone(offspring) // Clone individuals since an individual can be selected
+                                                // multiple times but it is still one object with multiple
+                                                // references to it.
 
+        for ((child1, child2) in offspring.pairs()) {    // Crossover
+            if (Random.nextDouble(1.0) < CX_PB) {  // offspring.pairs() -> (1,2), (3,4), (5,6), ...
+                crossover(child1, child2)
+                child1.fitness.invalidate()
+                child2.fitness.invalidate()
+            }
+        }
+
+        for (child in offspring) {
+            if (Random.nextDouble(1.0) < MUT_PB) {  // Mutation
+                mutate(child)
+                child.fitness.invalidate()
+            }
+        }
+
+        pop.clear()
+        pop.addAll(offspring)
+
+        for (ind in pop)
+            if (!ind.fitness.valid)
+                ind.fitness.value = fitness(repr.splitToReal(ind.bits))  // Evaluate only the individuals that need it.
     }
 
+    val solution = pop.maxBy { it.fitness }
 
 }
