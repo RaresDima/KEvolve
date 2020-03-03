dcfc0012dc8f42fa18697b2914f2092bb8093cf0
Rares Dima <rares.dima96@yahoo.com>
2020-03-03 14:20:37 +0200

    Added generic selection to SelectRandomWithoutReplacement.
    Signed-off-by: Rares Dima(uidn7122) <rares.dima@continental-corporation.com>

--- src/selection/SelectRandomWithoutReplacement.kt
+++ src/selection/SelectRandomWithoutReplacement.kt
@@ -17,8 +17,18 @@ class SelectRandomWithoutReplacement: BaseSelection() {
      *
      * Selecting the same individual multiple times is not possible.
      *
+     * [getFitness] is only needed if your individuals are highly customized and do
+     * not implement any predefined Individual class from KEvolve (hence the need for
+     * a user-defined method of getting the fitness).
+     *
+     * If you are using a class that implements an Individual class then
+     * `Individual.fitness` will be used as a default way to compare fitness scores.
+     *
      * @param pop The population to select from.
      * @param k The number of individuals to select.
+     * @param getFitness
+     *  A function that takes an individual and return a comparable value (an object
+     *  that implements [Comparable]). In this case this function has no effect.
      *
      * @throws PopulationTooSmallException
      * If the size of the population is less than [k] since that would mean that there
@@ -27,7 +37,10 @@ class SelectRandomWithoutReplacement: BaseSelection() {
      * @throws SelectionTooSmallException
      * If [k] < 1 since it makes no sense to select 0 individuals.
      */
-    override operator fun <INDIVIDUAL: BaseIndividual<*>> invoke(pop: List<INDIVIDUAL>, k: Int): List<INDIVIDUAL> {
+    override operator fun <INDIVIDUAL, FITNESS: Comparable<FITNESS>>
+            invoke(pop: List<INDIVIDUAL>,
+                   k: Int,
+                   getFitness: (INDIVIDUAL) -> FITNESS): List<INDIVIDUAL> {
 
         if (pop.size < k)
             throw PopulationTooSmallException("pop size = ${pop.size} < k = $k")
